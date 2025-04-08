b56dfac400c7f45feb03e7e3de04e68ac0598f71
rory <rory1205@gmail.com>
2025-04-08 09:30:15 +0800

    Initial Commit

---
+++ hw9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw9
+//
+//  Created by Rory on 2025/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
